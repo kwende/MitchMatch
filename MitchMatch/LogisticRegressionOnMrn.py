d8f2d634586d5da841e44097bd62fc54e63002b8
Ben <ben@ben-rush.net>
2017-08-01 16:11:53 -0500

    learning.

--- MitchMatch/LogisticRegressionOnMrn.py
+++ MitchMatch/LogisticRegressionOnMrn.py
@@ -4,32 +4,36 @@ from nltk import edit_distance
 import numpy as np
 import pickle
 
+VectorLength = 10
+
 #http://www.dummies.com/programming/big-data/data-science/using-logistic-regression-in-python-for-data-science/
 def computeDeltaVector(allRows, i1, i2):
     firstRow = [r.strip() for r in allRows[i1]]
     secondRow = [r.strip() for r in allRows[i2]]
 
     firstName = handleRow(firstRow, secondRow, 0, '')
-    middleName = handleRow(firstRow, secondRow, 1, '')
+    #middleName = handleRow(firstRow, secondRow, 1, '')
     lastName = handleRow(firstRow, secondRow, 2, '')
-    suffix = handleRow(firstRow, secondRow, 3, '')
+    #suffix = handleRow(firstRow, secondRow, 3, '')
     gender = handleRow(firstRow, secondRow, 4, '')
     social = handleRow(firstRow, secondRow, 5, '0')
     dob = handleRow(firstRow, secondRow, 6, '')
     phone = handleRow(firstRow, secondRow, 7, '')
-    phone2 = handleRow(firstRow, secondRow, 8, '')
+    #phone2 = handleRow(firstRow, secondRow, 8, '')
     address1 = handleRow(firstRow, secondRow, 9, '')
-    address2 = handleRow(firstRow, secondRow, 10, '')
+    #address2 = handleRow(firstRow, secondRow, 10, '')
     city = handleRow(firstRow, secondRow, 11, '')
     state = handleRow(firstRow, secondRow, 12, '')
     zip = handleRow(firstRow, secondRow, 13, '-1')
-    mothersMaidenName = handleRow(firstRow, secondRow, 14, '')
-    email = handleRow(firstRow, secondRow, 15, '')
-    alias = handleRow(firstRow, secondRow, 18, '')
+    #mothersMaidenName = handleRow(firstRow, secondRow, 14, '')
+    #email = handleRow(firstRow, secondRow, 15, '')
+    #alias = handleRow(firstRow, secondRow, 18, '')
 
-    goodVector = [firstName, middleName, lastName, suffix, gender, social, dob, phone, phone2, 
-            address1, address2, city, state, zip, mothersMaidenName, email, alias]
-    return goodVector
+    #goodVector = [firstName, middleName, lastName, suffix, gender, social, dob, phone, phone2, 
+    #        address1, address2, city, state, zip, mothersMaidenName, email, alias]
+
+    vector = [firstName, lastName, gender, social, dob, phone, address1, city, state, zip]
+    return vector
 
 def handleRow(firstRow, secondRow, index, blankString):
 
@@ -44,6 +48,12 @@ def handleRow(firstRow, secondRow, index, blankString):
         distance = -1
     else:
         distance = edit_distance(col1, col2)
+        maxEditDistance = 0 
+        if len(col1) > len(col2):
+           maxEditDistance = len(col1)
+        else:
+            maxEditDistance = len(col2)
+        distance = distance / maxEditDistance
 
     return distance
 
@@ -54,24 +64,35 @@ def Train(inputFile, savedOutput):
         allRows = [r for r in csvReader]
 
         numPairs = int(len(allRows) / 3)
-        vectors = np.zeros(shape=(numPairs * 2 - 1, 17))
+        #vectors = np.zeros(shape=(numPairs * 2 - 1, VectorLength))
         #goodVectors = np.zeros(shape=(numPairs, 17))
         #badVectors = np.zeros(shape=(numPairs - 1, 17))
+        goodVectors = []
+        badVectors = []
         y = []
 
         print("Scanning...")
         for i in range(0, numPairs):
+            if i % 100 == 0:
+                print(str(i) + " of " + str(numPairs))
             if len(allRows[i * 3]) == 19 and len(allRows[i * 3 + 1]) == 19:
                 goodVector = computeDeltaVector(allRows, i * 3, i * 3 + 1)
-                vectors[i * 2] = goodVector 
-                y.append(1)
-                #goodVectors[i] = goodVector
+                goodVectors.append(goodVector)
+                for j in range(0, numPairs, 10):
+                    if i != j:
+                        badVector = computeDeltaVector(allRows, i * 3, (j) * 3)
+                        badVectors.append(badVector)
 
-                if i < numPairs - 1:
-                    badVector = computeDeltaVector(allRows, i * 3, (i + 1) * 3)
-                    #badVectors[i] = badVector
-                    vectors[i * 2 + 1] = badVector
-                    y.append(0)
+        vectors = np.zeros(shape=(len(goodVectors) + len(badVectors), VectorLength))
+        m = 0
+        for goodVector in goodVectors:
+            vectors[m] = goodVector
+            y.append(1)
+            m = m + 1
+        for badVector in badVectors:
+            vectors[m] = badVector
+            y.append(2)
+            m = m + 1
 
         print("Learning...")
         logit = LogisticRegression()
@@ -90,6 +111,7 @@ def Match(inputFile, trainedFile):
     with open(inputFile) as csvFile:
         csvReader = csv.reader(csvFile)
 
+        count = 0
         possibles = []
         allRows = [r for r in csvReader]
         for i in range(1, len(allRows)):
@@ -101,14 +123,14 @@ def Match(inputFile, trainedFile):
             c = logit.predict(npDeltaVector.reshape(1, -1))
 
             if(c == 1):
-                possibles.append(allRows[i])
+               count = count + 1
 
+        print("Found " + str(count) + " matches"); 
     return
 
 def main():
-    #Train("c:/users/brush/desktop/mrns.csv",
-    #"c:/users/brush/desktop/learnedModel.pickle")
-    Match("c:/users/brush/desktop/logit/remaining.csv", "C:/users/brush/desktop/logit/learnedModel.pickle")
+    Train("c:/users/brush/desktop/logit/mrns.csv", "c:/users/brush/desktop/logit/learnedModel.pickle")
+    #Match("c:/users/brush/desktop/logit/remaining.csv", "C:/users/brush/desktop/logit/learnedModel.pickle")
     return
 
 
