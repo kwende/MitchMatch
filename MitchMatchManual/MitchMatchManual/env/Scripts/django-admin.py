263259ec4c6fd0afc9dba658feea6f61cf23bead
Ben <ben@ben-rush.net>
2017-08-06 23:02:40 -0500

    Website.

---
+++ MitchMatchManual/MitchMatchManual/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\repos\MitchMatch\MitchMatchManual\MitchMatchManual\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
