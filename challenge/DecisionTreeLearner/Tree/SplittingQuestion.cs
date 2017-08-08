8af9aa27ffc7c1df199fc060b608579e56ee5cd1
Ben <ben@ben-rush.net>
2017-08-08 12:22:57 -0500

    Starting the learner.

---
+++ challenge/DecisionTreeLearner/Tree/SplittingQuestion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTreeLearner.Tree
+{
+    public class SplittingQuestion
+    {
+        public int FieldIndex { get; set; }
+        public int AllowableDistance { get; set; }
+
+    }
+}
