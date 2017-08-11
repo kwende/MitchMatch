3aba689b3d259256b20b248083eda146d10637a8
Ben <ben@ben-rush.net>
2017-08-11 14:05:05 -0500

    starting address cleanup code.

---
+++ challenge/DecisionTreeLearner/NLP/DataCleaner.cs
@@ -0,0 +1,29 @@
+﻿using DecisionTreeLearner.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTreeLearner.NLP
+{
+    public static class DataCleaner
+    {
+        private static List<Tuple<string, string>> _suffixes = new List<Tuple<string, string>>(); 
+
+        //private static LazyLoadStreetSuffixes()
+        //{
+
+        //}
+
+        //public static Record CleanRecord(Record input)
+        //{
+        //    LazyLoadStreetSuffixes(); 
+
+        //    string cardinalCleaning =
+        //        input.Address1.ToUpper().Replace(" WEST ", " W ").Replace(" EAST ", " E ").Replace(" NORTH ", " N ").Replace(" SOUTH ", " S ");
+        //}
+
+
+    }
+}
