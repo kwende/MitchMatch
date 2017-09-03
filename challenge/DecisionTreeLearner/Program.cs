482a0bd108231ba9c49b069243ee0e864088b57d
Ben <ben@ben-rush.net>
2017-09-03 10:27:52 -0500

    Searching for reasons.

--- challenge/DecisionTreeLearner/Program.cs
+++ challenge/DecisionTreeLearner/Program.cs
@@ -86,7 +86,44 @@ namespace DecisionTreeLearner
             MySQLConnector conn = MySQLConnector.Connect();
             List<SetWithPossibleOthers> setsWithOthers = conn.GetSetsWithPossibleOthers();
 
-            return; 
+            DecisionTree[] forest = DataLoader.LoadForestFromDirectory("c:/users/brush/desktop/forest/");
+            List<RecordPair> pairs = DataLoader.BuildTrainingData("mrns.csv", "more.csv", "rejected.txt");
+
+            foreach (SetWithPossibleOthers setWithOther in setsWithOthers)
+            {
+                foreach (Record recordInSet in setWithOther.OriginalSet)
+                {
+                    foreach (Record possibleMatch in setWithOther.PossibleMatches)
+                    {
+                        RecordPair pair = new RecordPair
+                        {
+                            Record1 = recordInSet,
+                            Record2 = possibleMatch
+                        };
+
+                        TreeLogger logger = new TreeLogger();
+                        if (DecisionTreeBuilder.IsMatch(pair, forest, logger))
+                        {
+                            using (StreamWriter sw = File.AppendText(
+                                $"c:/users/brush/desktop/ends/{Guid.NewGuid().ToString().Replace("-", "")}.txt"))
+                            {
+                                Parallel.ForEach(pairs, p =>
+                                {
+                                    if (DecisionTreeBuilder.ReplayDecision(p, logger.SplittingQuestionsToTheBottom))
+                                    {
+                                        lock (sw)
+                                        {
+                                            sw.WriteLine(p);
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+
+            return;
 
             //Train(1, "C:/users/brush/desktop/forest", 1, 0, 3);
             //TestOnTrainingData();
